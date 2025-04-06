106b01e8cb2fbcc9ff17956707bffc3bd6e94bf8
rfo97 <rfa.alobaid@gmail.com>
2025-04-06 11:35:34 +0300

    added new changes

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
-
+import { ContentComponent } from '../content/content.component';
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent, SidebarComponent],
+  imports: [HeaderComponent, SidebarComponent, ContentComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
